4673bb4d74b2d43794ecfc9209ec00e4e47ab27d
aoria1 <aoria1@live.com>
2016-10-04 16:49:10 -0500

    Main_Page

--- src/GUI/StartUp.java
+++ src/GUI/StartUp.java
@@ -1,6 +1,10 @@
 package GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
-import java.awt.*;
+
 
 public class StartUp  {
 	private static JTextField emailTF = new JTextField();
@@ -8,11 +12,12 @@ public class StartUp  {
 	private static JLabel titleLabel = new JLabel("eProduce");
 	private static JLabel emailLabel = new JLabel("Email");
 	private static JLabel passwordLabel = new JLabel("Password");
-	private static JButton loginButton = new JButton("login");
+	private static JButton loginButton = new JButton("Login");
 	private static JButton signUpButton = new JButton("Sign Up");
+	public static JFrame frame = new JFrame("eProduce");
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JFrame frame = new JFrame("eProduce");
+	
 		frame.setSize(500, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
@@ -28,35 +33,46 @@ public class StartUp  {
 	private static void placeComponents(JPanel panel) {
 
 		panel.setLayout(null);
-
-	/**
-	 * Created Labels
-	 */
+	
+		/**
+		 * Create Labels
+		 */
+	
 		titleLabel.setBounds(150,10,150,150);
 		titleLabel.setFont(titleLabel.getFont().deriveFont(30f));
 		panel.add(titleLabel);
 		
-		emailLabel.setBounds(10, 10, 80, 25);
+		emailLabel.setBounds(100, 110, 80, 25);
 		panel.add(emailLabel);
 
 	
-		emailTF.setBounds(100, 10, 160, 25);
+		emailTF.setBounds(145, 110, 160, 25);
 		panel.add(emailTF);
 
 		
-		passwordLabel.setBounds(10, 40, 80, 25);
+		passwordLabel.setBounds(80, 140, 80, 25);
 		panel.add(passwordLabel);
 
 	
-		passwordPF.setBounds(100, 40, 160, 25);
+		passwordPF.setBounds(145, 140, 160, 25);
 		panel.add(passwordPF);
 
 		
-		loginButton.setBounds(10, 80, 80, 25);
+		loginButton.setBounds(140, 170, 80, 25);
 		panel.add(loginButton);
+		/**
+		 * Sends you to MainPage
+		 */
+		loginButton.addActionListener(new ActionListener() {
+	         public void actionPerformed(ActionEvent e) {
+	            frame.setVisible(false);
+	           
+	            MainPage mp = new MainPage();
+	            mp.createMain();
+	         }          
+	      });
 		
-		
-		signUpButton.setBounds(180, 80, 80, 25);
+		signUpButton.setBounds(230, 170, 80, 25);
 		panel.add(signUpButton);
 	}
 	
