918744efe48cace899c805345fb92c208ea8e732
Andrew Smith <asmith34082gmail.com>
2016-11-10 18:49:10 -0600

    Fixed bug where on main page load there was an index out of bound exception.

--- src/view/MainPageView.java
+++ src/view/MainPageView.java
@@ -393,7 +393,7 @@ public class MainPageView {
 		
 		for(int i = 0; i < listings.length; i++)
 		{
-			for(int j = 0; j < listings.length; j++)
+			for(int j = 0; j < listings[i].length; j++)
 				listings[i][j].setEditable(false);
 		}
 		
